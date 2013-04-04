ad540f739ceabe7f1aea7644200540e68f814e26
Rolland Thomas <rthomas@familysearch.org>
2013-04-04 13:52:40 -0600

    update3 for test.html

--- fs-dashboard-pgm.js
+++ fs-dashboard-pgm.js
@@ -3,7 +3,7 @@ var srv = require('express');
 var app = srv.createServer(srv.logger());
 
 app.get('fs-dashboard-pgm', function(request, response) {
-  response.sendfile('test.html');
+  response.send('test.html');
 });
 
 var port = process.env.PORT || 5000;
