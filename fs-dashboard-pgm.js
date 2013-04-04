67b02ef7dc0571c99ca4c33d01e04ea7a5eb7b62
Rolland Thomas <rthomas@familysearch.org>
2013-04-04 13:47:58 -0600

    update2 for test.html

--- fs-dashboard-pgm.js
+++ fs-dashboard-pgm.js
@@ -2,7 +2,7 @@ var srv = require('express');
 
 var app = srv.createServer(srv.logger());
 
-app.get('/fs-dashboard-pgm', function(request, response) {
+app.get('fs-dashboard-pgm', function(request, response) {
   response.sendfile('test.html');
 });
 
