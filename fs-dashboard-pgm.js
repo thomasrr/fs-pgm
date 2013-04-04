525fce2f7cbc4da03fb641c197c18e8dbf8aebab
Rolland Thomas <rthomas@familysearch.org>
2013-04-04 13:37:20 -0600

    testing Heroku deploy

--- fs-dashboard-pgm.js
+++ fs-dashboard-pgm.js
@@ -1,13 +1,10 @@
-var express = require('express');
+var srv = require('express');
 
-var app = express.createServer(express.logger());
+var app = srv.createServer(srv.logger());
 
-app.get('/', function(request, response) {
-  res.type('.html');
-  res.sendfile('C:/Users/rrthomas/Desktop/Projects/Code/', 'metricsDashboard.html');
+app.get('/fs-dashboard-pgm', function(request, response) {
+  response.sendfile('test.html');
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port);
