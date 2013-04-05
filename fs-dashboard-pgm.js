ab2fef00d6625287c8ca6fd9c6df62de7022a770
Rolland Thomas <rthomas@familysearch.org>
2013-04-04 19:08:29 -0600

    testing heroku deploy

--- fs-dashboard-pgm.js
+++ fs-dashboard-pgm.js
@@ -6,6 +6,7 @@ app.use(express.static(__dirname + '\\public'));
 //app.set('view engine', 'html');
 //app.set('views', __dirname);
 
+console.log ("testing 123 ....");
 
 var port = process.env.PORT || 5000;
 app.listen(port);
