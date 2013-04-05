7aa024a9c52555ac8ad0a01cb722854d5b2d8b1b
Rolland Thomas <rthomas@familysearch.org>
2013-04-05 13:19:45 -0600

    append Child

--- public/loginPage.js
+++ public/loginPage.js
@@ -4,26 +4,31 @@
  var descr = document.createElement('div');
  descr.setAttribute('class', 'halfPage');
  descr.setAttribute('value', 'This will hold a description related to Metrics Dashboard including new features.');
+ document.body.appendChild(descr);
  
  var userName = document.createElement('input');
  userName.setAttribute('type', 'text');
  userName.setAttribute('class', 'userInput');
+ document.body.appendChild(descr);
  
  var password = document.createElement('input');
  password.setAttribute('type', 'password');
  password.setAttribute('class', 'userInput');
+ document.body.appendChild(descr);
  
  var login = document.createElement('input');
  login.setAttribute('type', 'button');
  login.setAttribute('value', 'Login');
  login.setAttribute('id', 'login');
  login.onClick = processLogin;
+ document.body.appendChild(descr);
  
  var cancel = document.createElement('input');
  cancel.setAttribute('type', 'button');
  cancel.setAttribute('value', 'Cancel');
  cancel.setAttribute('id', 'cancel');
  cancel.onClick = processCancel;
+ document.body.appendChild(descr);
  
 }
 
