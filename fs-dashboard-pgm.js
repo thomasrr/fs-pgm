5e4e570a33746169ba0ecac90ab76f9c65a2166a
Rolland Thomas <rthomas@familysearch.org>
2013-04-05 11:50:56 -0600

    small update

--- fs-dashboard-pgm.js
+++ fs-dashboard-pgm.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-//app.set('view engine', 'html');
-//app.set('views', __dirname);
+//app.use(express.basicAuth('fs-pgm', 'dashboard'));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
