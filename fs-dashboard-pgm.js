0c6d9ee947e741f8dd411700accea621f3cd9661
Rolland Thomas <rthomas@familysearch.org>
2013-05-03 08:08:02 -0600

    mongo HQ fix

--- fs-dashboard-pgm.js
+++ fs-dashboard-pgm.js
@@ -1,10 +1,9 @@
 var express = require('express');
-var fsData = require('./public/fs-pgm-data');
+var fsData = require('/public/fs-pgm-data');
 
 var app = express();
 
 app.use(express.static('/public'));
-//app.use(express.basicAuth('fs-pgm', 'dashboard'));
 
 app.configure(function () {
   app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
